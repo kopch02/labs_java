cda9df8ffa99f1f33346e951049ee3f2deddbc27
kopch <kopch08@gmail.com>
2022-04-04 11:06:34 +0700

    java_lab3_num2

--- lab3/num_2/src/main/java/com/example/num_2/HelloController.java
+++ lab3/num_2/src/main/java/com/example/num_2/HelloController.java
@@ -7,8 +7,45 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        while (true) {
+            welcomeText.setText("0");
+        }
+    }
+    @FXML
+    protected void potoci() {
+        Potoc_print pp = new Potoc_print();
+        Potoc potoc_1 = new Potoc(pp);
+        potoc_1.start();
+    }
+}
+class Potoc_print{
+    @FXML
+    Label potoc_text=new Label();
+    int x=0;
+    @FXML
+    protected void potoci_print() {
+
+        potoc_text.setText("123" + x);
+        x++;
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException e){
+        }
+
+    }
+}
+class Potoc extends Thread{
+    Potoc_print pp;
+    Potoc(Potoc_print pp){
+        this.pp=pp;
+    }
+    public void run(){
+        while (true) {
+            pp.potoci_print();
+        }
     }
 }
